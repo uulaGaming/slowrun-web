e4d6325680daf1dffc0f759f86abcf22856ed9c4
uula <us3411@edu.turku.fi>
2026-08-19 13:16:21 +0000

    started with important work

---
+++ site.js
@@ -0,0 +1,42 @@
+/*
+**  site.js
+**
+** awesome swaggy backend for a
+** stupid fake site
+**
+*/
+
+/* import this shit */
+import * as http from "node:http";
+import * as fs from "node:fs";
+
+/* declare server constants */
+const hostname = '127.0.0.1';
+const port = 8080;
+
+/* make the server with with a handler function */
+const server = http.createServer((req, res) => {
+
+	/* check the requested page */
+	switch(req.url){
+
+		case "/":
+			res.statusCode = 200;
+			res.setHeader("Content-Type","text/html");
+			res.write("<h1>mega swagballs</h1>");
+			res.end("<p>yuhuh</p>");
+			break;
+
+		/* only requests from dumbasses will end up here */
+		default:
+			res.statusCode = 404;
+			res.setHeader("Content-Type","text/plain");
+			res.end("idk man");
+			break;
+	}
+});
+
+/* start the server */
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
