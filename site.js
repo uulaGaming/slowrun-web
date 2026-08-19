2af69e39a83f38119b3a8366da32f7200fbe8c5b
uula <us3411@edu.turku.fi>
2026-08-19 14:07:08 +0000

    made files work slightly better

--- site.js
+++ site.js
@@ -9,10 +9,26 @@
 /* import this shit */
 import * as http from "node:http";
 import * as fs from "node:fs";
+import * as path from "node:path";
 
 /* declare server constants */
 const hostname = '127.0.0.1';
 const port = 8080;
+
+const STATICPATH = path.join(process.cwd(), "./static");
+
+/* yes this is stolen from mdn */
+const MIME_TYPES = {
+  default: "application/octet-stream",
+  html: "text/html; charset=UTF-8",
+  js: "text/javascript",
+  css: "text/css",
+  png: "image/png",
+  jpg: "image/jpeg",
+  gif: "image/gif",
+  ico: "image/x-icon",
+  svg: "image/svg+xml",
+};
 
 /* function for reading a file and sending it as a response */
 function respondFile(file, res){
@@ -23,10 +39,12 @@ const server = http.createServer((req, res) => {
 
 	res.statusCode = 200;
 
-	/* check the requested page */
-	switch(req.url){
+	const url = req.url;
 
-		case "test":
+	/* check the requested page */
+	switch(url){
+
+		case "/test":
 			res.setHeader("Content-Type","text/html");
 			res.write("<h1>mega swagballs</h1>");
 			res.end("<p>yuhuh</p>");
@@ -35,10 +53,17 @@ const server = http.createServer((req, res) => {
 		/* oops */
 		default:
 			try{
-				const data = fs.readFileSync("index.html", 'utf8');
-				res.write(data);
+				/* look for file */
+				let filepath = STATICPATH;
+				filepath += decodeURI(url);
+				if (url == "/") filepath += "index.html";
+				const ext = path.extname(filepath).substring(1).toLowerCase();
+				const mtype = MIME_TYPES[ext] || MIME_TYPES.default;
+				res.setHeader("Content-Type",mtype);
+				res.write(fs.readFileSync(filepath, 'utf8'));
 				res.end();
 			} catch(err){
+				/* there was no file */
 				console.log(err);
 				res.statusCode = 404;
 				res.setHeader("Content-Type","text/plain");
